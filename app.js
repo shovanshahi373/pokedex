e8c2726c4eab05834dbdb9b12f7dc55ac48c4d0a
Awekening_bro <37173948+shovanshahi373@users.noreply.github.com>
2019-12-03 22:21:01 +0545

    Update app.js

--- app.js
+++ app.js
@@ -7,82 +7,6 @@ const client = require("./config/db");
 const pokeFetch = require("./config/pokeFetch");
 const AdvancedSearch = require("./config/advancedSearch");
 
-// fs.exists("./resources/json/pokeimage.json", exists => {
-//   if (!exists) {
-//     fs.readdir("./resources/images/3dSprites", (err, file) => {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         console.log(path.join("./images/3dSprites/", path.basename(file[1])));
-//         let data = [];
-//         file.forEach(image => {
-//           data.push({
-//             sprite: `./images/3dSprites/${path.basename(image)}`
-//           });
-//         });
-
-//         fs.writeFile(
-//           "./resources/json/pokeimage.json",
-//           JSON.stringify(data),
-//           err => {
-//             if (err) {
-//               console.log("an error occured!");
-//             } else {
-//               console.log("successfully created json file!");
-//             }
-//           }
-//         );
-//       }
-//     });
-//   } else {
-//     console.log("the file already exists!!");
-//   }
-// });
-
-// fs.exists("./resources/json/pokeimagepng.json", exists => {
-//   if (!exists) {
-//     fs.readdir("./resources/images/png", (err, file) => {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         console.log(path.join("./images/png/", path.basename(file[1])));
-//         let data = [];
-//         file.forEach(image => {
-//           data.push({
-//             sprite: `./images/png/${path.basename(image)}`
-//           });
-//         });
-
-//         fs.writeFile(
-//           "./resources/json/pokeimagepng.json",
-//           JSON.stringify(data),
-//           err => {
-//             if (err) {
-//               console.log("an error occured!");
-//             } else {
-//               console.log("successfully created json file!");
-//             }
-//           }
-//         );
-//       }
-//     });
-//   } else {
-//     console.log("the file already exists!!");
-//   }
-// });
-
-// const newpkmn = pkmn.map((p,i) => {
-//   let newe = `${pokeImg[i].sprite}`;
-//   p["sprite"] = newe;
-//   return p;
-// })
-
-// if(!fs.existsS("./resources/json/complete.json"))
-// fs.writeFile("./resources/json/completePokeInfo.json", JSON.stringify(newpkmn),(err) => {
-//   if(err) throw err;
-//   console.log("successfully created complete pokedex!");
-// })
-
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -109,5 +33,5 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, err => {
   if (err) console.log("error connecting to server");
-  console.log("server is running in port 3000...");
+  console.log(`server is running in port ${PORT}...`);
 });
