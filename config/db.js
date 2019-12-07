f361431e1dba763bbf0e30d97344d1b3399796b1
sovan <sovanshahihero@gmail.com>
2019-12-07 12:12:00 +0545

    v3.1

--- config/db.js
+++ config/db.js
@@ -2,16 +2,24 @@ const mongoClient = require("mongodb").MongoClient;
 let string;
 if (process.env.NODE_ENV == "production") {
   string = `mongodb+srv://${process.env.USER}:${process.env.KEY}@cluster0-vpwzk.mongodb.net/test?retryWrites=true&w=majority`;
+  client = new mongoClient(string, {
+    useNewUrlParser: true
+    // useUnifiedTopology: true
+  });
 } else {
   const {
     general: { name, pass }
   } = require("./key");
   string = `mongodb+srv://${name}:${pass}@cluster0-vpwzk.mongodb.net/test?retryWrites=true&w=majority`;
+  client = new mongoClient(string, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 }
-client = new mongoClient(string, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+// client = new mongoClient(string, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
 
 client.connect((err, client) => {
   if (err) {
